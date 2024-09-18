5afab49f32f8581e23efbfffb4fdabae6759402c
Michal Radomski <michalra@op.pl>
2024-09-18 14:02:23 +0200

    Geocoding Project - initial commit2

--- geocoding-project/vite.config.ts
+++ geocoding-project/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import sass from "sass";
+import sass from "sass-embedded";
 import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/

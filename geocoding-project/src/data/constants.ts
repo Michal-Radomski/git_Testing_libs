656e3b9ca5e01dbe79d54b4bb7a9cf0461f20cad
Michal Radomski <michalra@op.pl>
2024-09-19 11:45:39 +0200

    Testing done

--- geocoding-project/src/data/constants.ts
+++ geocoding-project/src/data/constants.ts
@@ -7,15 +7,21 @@
 // export const initialPosition = { latitude: 54.40414, longitude: 18.60747 };
 
 //* V2
-export const testPoints = [
-  [52.41133, 16.931278],
-  [52.24809, 17.08797],
-  [52.086425, 17.020075],
-  [52.229837, 17.272707],
-];
+// export const testPoints = [
+//   [52.41133, 16.931278],
+//   [52.24809, 17.08797],
+//   [52.086425, 17.020075],
+//   [52.229837, 17.272707],
+// ];
+// export const initialPosition = { latitude: 52.24809, longitude: 17.08797 };
 
-export const initialPosition = { latitude: 52.24809, longitude: 17.08797 };
+//* V3
+export const testPoints = [
+  [52.402608, 16.913718],
+  [52.399992, 16.916075],
+];
+export const initialPosition = { latitude: 52.399992, longitude: 16.916075 };
 
 export const redOptions = { color: "red" };
-export const zoom = 10.5;
+export const zoom = 16.5;
 export const radius = 5;

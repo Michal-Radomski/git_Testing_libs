8ad675e5a491e3212f3c69eed25b11c856e2a6cb
Michal Radomski <michalra@op.pl>
2024-10-22 19:10:27 +0200

    Keycloak was added

--- 05_keycloak/src/App.tsx
+++ 05_keycloak/src/App.tsx
@@ -1,9 +1,45 @@
 import React from "react";
+import { ReactKeycloakProvider, useKeycloak } from "@react-keycloak/web";
+import Keycloak from "keycloak-js";
 
 import "./App.scss";
 
+const realm = import.meta.env.VITE_Keycloak_realm as string;
+const url = import.meta.env.VITE_Keycloak_url as string;
+const clientId = import.meta.env.VITE_Keycloak_clientId as string;
+// console.log({ realm, url, clientId });
+
+const keycloak = new Keycloak({
+  url: url,
+  realm: realm,
+  clientId: clientId,
+});
+
 const App = (): JSX.Element => {
-  return <React.Fragment>App</React.Fragment>;
+  const { keycloak, initialized } = useKeycloak();
+
+  if (!initialized) {
+    return <div>Loading...</div>;
+  }
+
+  if (!keycloak.authenticated) {
+    return <div>Not authenticated</div>;
+  }
+
+  return (
+    <React.Fragment>
+      <div>
+        <p>Welcome, {keycloak.tokenParsed?.name}</p>
+        <button onClick={() => keycloak.logout()}>Logout</button>
+      </div>
+    </React.Fragment>
+  );
 };
 
-export default App;
+const WrappedApp = (): JSX.Element => (
+  <ReactKeycloakProvider authClient={keycloak}>
+    <App />
+  </ReactKeycloakProvider>
+);
+
+export default WrappedApp;

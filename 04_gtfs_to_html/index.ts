713e766479d171ac656dad98a2c516c3e170d50e
Michal Radomski <michalra@op.pl>
2024-10-22 15:38:57 +0200

    gtfs_to_html - initial commit

---
+++ 04_gtfs_to_html/index.ts
@@ -0,0 +1,26 @@
+// import gtfsToHtml from "gtfs-to-html";
+
+// console.log("gtfsToHtml:", gtfsToHtml);
+
+// const config = {
+//   agencies: [
+//     {
+//       agencyKey: "Kombus",
+//       path: "https://iplaner.pl/gtfs/kombus.zip",
+//     },
+//   ],
+//   logFunction: function (text: string) {
+//     // Do something with the logs here, like save it or send it somewhere
+//     console.log(text);
+//   },
+// };
+
+// gtfsToHtml(config)
+//   .then(() => {
+//     console.log("HTML Generation Successful");
+//     process.exit();
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//     process.exit(1);
+//   });

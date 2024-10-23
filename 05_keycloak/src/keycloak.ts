2238c15ef4be3dc192df0de70e26a41afd03542e
Michal Radomski <michalra@op.pl>
2024-10-23 16:52:47 +0200

    React client updated

--- 05_keycloak/src/keycloak.ts
+++ 05_keycloak/src/keycloak.ts
@@ -1,16 +1,18 @@
-import Keycloak from "keycloak-js";
+import Keycloak, { KeycloakConfig } from "keycloak-js";
 
 const realm = import.meta.env.VITE_Keycloak_realm as string;
 const url = import.meta.env.VITE_Keycloak_url as string;
 const clientId = import.meta.env.VITE_Keycloak_clientId as string;
 // console.log({ realm, url, clientId });
 
-const keycloak = new Keycloak({
+const initOptions = {
   url: url,
   realm: realm,
   clientId: clientId,
-  // onLoad: 'check-sso', // check-sso | login-required
-  // KeycloakResponseType: 'code',
-});
+  onLoad: "check-sso", // check-sso | login-required
+  KeycloakResponseType: "code",
+} as KeycloakConfig;
+
+const keycloak = new Keycloak(initOptions);
 
 export default keycloak;

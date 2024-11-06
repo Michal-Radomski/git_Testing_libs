19032146550badadd71b8f88cedb7e8a140cca23
Michal Radomski <michalra@op.pl>
2024-11-06 11:57:41 +0100

    GTFS package tested

--- 04_gtfs_to_html_(A)/index.ts
+++ 04_gtfs_to_html_(A)/index.ts
@@ -1,6 +1,7 @@
 // import gtfsToHtml from "gtfs-to-html";
 // import gtfsToGeoJSON from "gtfs-to-geojson";
-import gtfsToChart from "gtfs-to-chart";
+// import gtfsToChart from "gtfs-to-chart";
+import { importGtfs, getAgencies, openDb, closeDb } from "gtfs";
 
 //* 1. GTFS to HTML
 // const config = {
@@ -48,26 +49,45 @@ import gtfsToChart from "gtfs-to-chart";
 //     console.error(err);
 //   });
 
-//* 3. GTFS to Chart
+//* 4. GTFS
 const config = {
+  sqlitePath: "./db.sqlite3",
   agencies: [
     {
-      agency_key: "Kombus",
       path: "./kombus.zip",
     },
   ],
+  verbose: true,
   logFunction: function (text: string) {
     // Do something with the logs here, like save it or send it somewhere
-    console.log(text);
+    console.log("text:", text);
   },
 };
 
-gtfsToChart(config)
-  .then(() => {
-    console.log("Chart Generation Successful");
-    process.exit();
-  })
-  .catch((err: unknown) => {
-    console.error(err);
-    process.exit(1);
-  });
+try {
+  (async function (): Promise<void> {
+    await importGtfs(config)
+      .then(() => {
+        console.log("Successful");
+        // process.exit();
+      })
+      .catch((err: unknown) => {
+        console.error(err);
+        process.exit(1);
+      })
+      .finally(() => {
+        (async function displayAgencies(): Promise<void> {
+          const db = await openDb(config);
+          const dbConfig = { db: db };
+          const agencies = await getAgencies({}, [], [], dbConfig);
+          console.log({ agencies });
+          await closeDb(db);
+          process.exit();
+        })();
+      });
+  })();
+
+  // Get all agencies
+} catch (error) {
+  console.log("error:", error);
+}

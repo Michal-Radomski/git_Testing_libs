e5afb87252e158822712d7f9bdc5300da536446d
Michal Radomski <michalra@op.pl>
2025-03-21 17:06:33 +0100

    Testing transitive.js and PolylineOffset done

--- 06_transitive_offset (A)/src/helpers.ts
+++ 06_transitive_offset (A)/src/helpers.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 
+import { Points, PointsData } from "./Interfaces";
+
 export const getRouteData = async (url: string): Promise<PointsData> => {
   const { data } = await axios.get(url);
 
   const points_0 = data?.routes?.[0]?.shape?.[0].loc as Points[];
   const points_1 = data?.routes?.[0]?.shape?.[1].loc as Points[];
-  const color = `#${data?.routes?.[0]?.color}`;
+  const color: string = `#${data?.routes?.[0]?.color}`;
   // console.log({ color });
 
   return { points_0, points_1, color };
